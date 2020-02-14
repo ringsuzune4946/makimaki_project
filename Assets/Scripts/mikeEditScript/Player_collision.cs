7ed9b076ac2ead7957bde43351f09617bc6a8160
mike <mkta08.you12@gmail.com>
2020-02-14 23:27:57 +0900

    プレイヤーの移動動作確認のためのマップとプレイヤー(プロトタイプ)を配置 (糞コードだから組みなおす可能性高)

---
+++ Assets/Scripts/mikeEditScript/Player_collision.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_collision : MonoBehaviour
+{
+
+    [SerializeField] bool isTriggerStay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isTriggerStay = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "collisionmap")
+        {
+            isTriggerStay = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "collisionmap")
+        {
+            isTriggerStay = false;
+        }
+    }
+
+    public bool Trigger_check()
+    {
+        return this.isTriggerStay;
+    }
+}
